632df121236b2948dbbf3c763a64d74d8622907f
Jan Seeger <jan@alphadev.net>
2014-09-04 14:30:57 +0200

    parse capacity header length.

--- app/src/main/java/net/alphadev/usbstorage/scsi/ReadFormatCapacitiesHeader.java
+++ app/src/main/java/net/alphadev/usbstorage/scsi/ReadFormatCapacitiesHeader.java
@@ -6,11 +6,16 @@ package net.alphadev.usbstorage.scsi;
 public class ReadFormatCapacitiesHeader {
     public static final int LENGTH = 12;
 
+    private byte mCapacityListLength;
+
     public ReadFormatCapacitiesHeader(byte[] answer) {
+        /** first three bits are reserved **/
+        mCapacityListLength = answer[3];
+
 
     }
 
     public int getCapacityEntryCount() {
-        return 0;
+        return mCapacityListLength;
     }
 }
