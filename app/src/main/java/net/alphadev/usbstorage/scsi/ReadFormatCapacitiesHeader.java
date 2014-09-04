0900d5713a6c52ebc0993d44e230afb469a0af5d
Jan Seeger <jan@alphadev.net>
2014-09-04 14:36:58 +0200

    add numberof blocks of current/maximum capacity descriptor.

--- app/src/main/java/net/alphadev/usbstorage/scsi/ReadFormatCapacitiesHeader.java
+++ app/src/main/java/net/alphadev/usbstorage/scsi/ReadFormatCapacitiesHeader.java
@@ -1,5 +1,7 @@
 package net.alphadev.usbstorage.scsi;
 
+import static net.alphadev.usbstorage.util.BitStitching.convertToInt;
+
 /**
  * Created by jan on 29.08.14.
  */
@@ -7,12 +9,13 @@ public class ReadFormatCapacitiesHeader {
     public static final int LENGTH = 12;
 
     private byte mCapacityListLength;
+    private int mNumberOfBlocks;
+
 
     public ReadFormatCapacitiesHeader(byte[] answer) {
         /** first three bits are reserved **/
         mCapacityListLength = answer[3];
-
-
+        mNumberOfBlocks = convertToInt(answer, 4);
     }
 
     public int getCapacityEntryCount() {
