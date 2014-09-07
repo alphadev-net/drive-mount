e087156fc1d26b54084134b34882d24d6e125f2a
Jan Seeger <jan@alphadev.net>
2014-09-07 13:23:08 +0200

    implemented read capacity scsi command. add partially read capacity response parser.

---
+++ lib/src/main/java/net/alphadev/usbstorage/scsi/answer/ReadCapacityResponse.java
@@ -0,0 +1,8 @@
+package net.alphadev.usbstorage.scsi.answer;
+
+/**
+ * @author Jan Seeger <jan@alphadev.net>
+ */
+public class ReadCapacityResponse {
+    public static final int LENGTH = 8;
+}
---
+++ lib/src/main/java/net/alphadev/usbstorage/scsi/command/ReadCapacity.java
@@ -0,0 +1,55 @@
+package net.alphadev.usbstorage.scsi.command;
+
+import net.alphadev.usbstorage.bbb.CommandBlockWrapper;
+import net.alphadev.usbstorage.scsi.answer.ReadCapacityResponse;
+
+import static net.alphadev.usbstorage.util.BitStitching.setBytesFromInt;
+
+/**
+ * @author Jan Seeger <jan@alphadev.net>
+ */
+@SuppressWarnings("unused")
+public class ReadCapacity extends ScsiCommand {
+    public static final byte READ_CAPACITY = 0x25;
+
+    private int mLogicalBlockAddress;
+    private byte mControl;
+
+    public ReadCapacity() {
+        super(READ_CAPACITY);
+    }
+
+    @Override
+    public CommandBlockWrapper.Direction getDirection() {
+        return CommandBlockWrapper.Direction.DEVICE_TO_HOST;
+    }
+
+    @Override
+    public byte[] asBytes() {
+        byte[] retval = new byte[10];
+        retval[0] = READ_CAPACITY; // opcode
+        // retval[1] is reserved
+        setBytesFromInt(mLogicalBlockAddress, retval, 2);
+        // retval[6-8] is reserved
+        retval[9] = mControl;
+        return retval;
+    }
+
+
+    @Override
+    public int getExpectedAnswerLength() {
+        return ReadCapacityResponse.LENGTH;
+    }
+
+    public int getLogicalBlockAddress() {
+        return mLogicalBlockAddress;
+    }
+
+    public void setLogicalBlockAddress(int logicalBlockAddress) {
+        this.mLogicalBlockAddress = logicalBlockAddress;
+    }
+
+    public void setControl(byte control) {
+        this.mControl = control;
+    }
+}
