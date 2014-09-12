dcc108709d055c0f170c7860858b4110c501d403
Jan Seeger <jan@alphadev.net>
2014-09-12 19:45:32 +0200

    implemented basic modesense command.

--- lib/src/main/java/net/alphadev/usbstorage/scsi/command/ModeSense.java
+++ lib/src/main/java/net/alphadev/usbstorage/scsi/command/ModeSense.java
@@ -20,7 +20,12 @@ public class ModeSense extends ScsiCommand {
     @Override
     public byte[] asBytes() {
         byte[] retval = new byte[6];
-        retval[0] = MODE_SENSE;
+        retval[0] = MODE_SENSE; // opcode
+        // 1 == dbd
+        retval[2] = 0x3f;
+        // 3 == =0 Return all subpage 00h mode pages in page_0 format
+        retval[4] = (byte) 192;
+        // 5 == control bit
         return retval;
     }
 
