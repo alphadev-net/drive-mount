02d8ec26e7a90c4f47c0dd812fe13d9af43b7d74
Jan Seeger <jan@alphadev.net>
2014-11-02 15:09:19 +0100

    offset parameter specifies the offset in the dst array.

--- lib/src/main/java/net/alphadev/usbstorage/partition/MasterBootRecord.java
+++ lib/src/main/java/net/alphadev/usbstorage/partition/MasterBootRecord.java
@@ -32,9 +32,10 @@ public class MasterBootRecord {
         }
 
         for (int entry = 0; entry < 4; entry++) {
-            byte[] data = new byte[16];
+            byte[] data = new byte[PARTITION_DATA_LENGTH];
             int offset = PARTITION_DATA_OFFSET + entry * PARTITION_DATA_LENGTH;
-            buffer.get(data, offset, PARTITION_DATA_LENGTH);
+            buffer.position(offset);
+            buffer.get(data);
 
             PartitionParameters param = new PartitionParameters(data);
             Partition partition = new Partition(device, entry, param);
