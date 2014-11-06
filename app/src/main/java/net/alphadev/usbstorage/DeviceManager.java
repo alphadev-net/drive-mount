efcdec418c06f15202612f12a1749ed6c6e18c53
Jan Seeger <jan@alphadev.net>
2014-11-06 17:47:13 +0100

    pop a toast if the drive was successfully mounted.

--- app/src/main/java/net/alphadev/usbstorage/DeviceManager.java
+++ app/src/main/java/net/alphadev/usbstorage/DeviceManager.java
@@ -23,6 +23,7 @@ import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import net.alphadev.usbstorage.api.BulkDevice;
 
@@ -76,6 +77,7 @@ public final class DeviceManager {
         mStorageManager.tryMount(usbBulkDevice);
 
         notifyStorageChanged();
+        Toast.makeText(mContext, "Mounted USB Device", Toast.LENGTH_SHORT).show();
     }
 
     private void notifyStorageChanged() {
